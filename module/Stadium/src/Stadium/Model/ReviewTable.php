cf9e1fb226bc1d98be9d8ca38e11604104aa5369
yagovkin <roman@official.com.au>
2014-07-27 10:58:15 +1000

    ZendDeveloderTools added

--- module/Stadium/src/Stadium/Model/ReviewTable.php
+++ module/Stadium/src/Stadium/Model/ReviewTable.php
@@ -2,6 +2,9 @@
 namespace Stadium\Model;
 
 use Zend\Db\TableGateway\TableGateway;
+use Zend\Db\Sql\Select;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\Adapter\Adapter;
 
 class ReviewTable
 {
@@ -28,6 +31,63 @@ class ReviewTable
         }
         return $row;
     }
+
+
+
+
+
+
+    public function getReviews()
+    {
+        $select = new \Zend\Db\Sql\Select ;
+        $select->from('review');
+        $select->columns(array('*'));
+        $select->join('user', "user.id = review.reviewedby", array('*'), 'left');
+         
+        echo $select->getSqlString();
+        $resultSet = $this->tableGateway->selectWith($select);
+       
+        return $resultSet; 
+    }
+
+
+ /*   public function getReviews()
+    {
+       $dbAdapter = new Zend\Db\Adapter\Adapter(array(
+        'driver' => 'Mysqli',
+        'database' => 'rnr2db',
+        'username' => 'root',
+        'password' => 'root'
+        ));
+
+       $select = new Select();
+       $select->from('review')
+       ->columns(array('review.*', 'u_name' => 'user.username'))
+       ->join('user', 'review.reviewedby = user.id');
+
+       $statement = $dbAdapter->createStatement();
+       $select->prepareStatement($dbAdapter, $statement);
+       $driverResult = $statment->execute();
+
+       $resultset = new ResultSet();
+$resultset->initialize($driverResult); // can use setDataSource() for older ZF2 versions.
+
+//foreach ($resultset as $row) {
+        // $row is an ArrayObject
+
+return $resultset;
+}
+*/
+
+
+
+
+
+
+
+
+
+
 /*
     public function saveReview(Review $review)
     {
@@ -54,4 +114,5 @@ class ReviewTable
         $this->tableGateway->delete(array('id' => $id));
     }
 */    
+
 }
